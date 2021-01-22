1c9fbe05c1f568752ac44726e265ec248e7522a0
Jimmy <408224334@qq.com>
2021-01-22 20:24:19 +0800

    feat eraser apply ui

--- src/js/ui/template/submenu/eraser.js
+++ src/js/ui/template/submenu/eraser.js
@@ -3,18 +3,29 @@
  *   @param {Locale} locale - Translate text
  * @returns {string}
  */
-export default ({ locale }) => `
-  <ul class="tui-image-editor-submenu-item">
-    <li>
-        <div class="tie-eraser-color" title="${locale.localize('Color')}"></div>
-    </li>
-    <li class="tui-image-editor-partition only-left-right">
-        <div></div>
-    </li>
-    <li class="tui-image-editor-newline tui-image-editor-range-wrap">
-        <label class="range">${locale.localize('Range')}</label>
-        <div class="tie-eraser-range"></div>
-        <input class="tie-eraser-range-value tui-image-editor-range-value" value="0" />
-    </li>
-  </ul>
+export default ({ locale, makeSvgIcon }) => `
+    <ul class="tui-image-editor-submenu-item">
+        <li>
+            <div class="tie-eraser-color" title="${locale.localize('Color')}"></div>
+        </li>
+        <li class="tui-image-editor-partition only-left-right">
+            <div></div>
+        </li>
+        <li class="tui-image-editor-newline tui-image-editor-range-wrap">
+            <label class="range">${locale.localize('Range')}</label>
+            <div class="tie-eraser-range"></div>
+            <input class="tie-eraser-range-value tui-image-editor-range-value" value="0" />
+        </li>
+        <li class="tui-image-editor-partition only-left-right">
+            <div></div>
+        </li>
+        <li class="tie-eraser-apply tui-image-editor-newline apply" style="margin-top: 22px;margin-bottom: 5px">
+            <div class="tui-image-editor-button apply">
+                ${makeSvgIcon(['normal', 'active'], 'apply')}
+                <label>
+                    ${locale.localize('Apply')}
+                </label>
+            </div>
+        </li>
+    </ul>
 `;
